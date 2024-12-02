e5a03152f0678898ac3373dfe43d8ff15e8ad14b
Tarkiya <1334753765@qq.com>
2024-12-02 23:23:44 +0800

    -new5

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ HELP = 'Create         : create an conference\n' \
        'Cancel         : cancel your on-going conference (only the manager)\n\n'
 
 SERVER_IP = '127.0.0.1'
-MAIN_SERVER_PORT = 8888
+MAIN_SERVER_PORT = 8000
 TIMEOUT_SERVER = 5
 # DGRAM_SIZE = 1500  # UDP
 LOG_INTERVAL = 2
@@ -14,3 +14,6 @@ CHANNELS = 1  # Channels for audio capture
 RATE = 44100  # Sampling rate for audio capture
 
 camera_width, camera_height = 480, 480  # resolution for camera capture
+
+
+BUFFER_SIZE = 65536
