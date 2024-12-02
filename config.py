33d3eded1c40b7cf39edb3c176fa659205f0ad45
Tarkiya <1334753765@qq.com>
2024-12-02 23:23:44 +0800

    -new5

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ HELP = 'Create         : create an conference\n' \
        'Cancel         : cancel your on-going conference (only the manager)\n\n'
 
 SERVER_IP = '127.0.0.1'
-MAIN_SERVER_PORT = 8888
+MAIN_SERVER_PORT = 8000
 TIMEOUT_SERVER = 5
 # DGRAM_SIZE = 1500  # UDP
 LOG_INTERVAL = 2
